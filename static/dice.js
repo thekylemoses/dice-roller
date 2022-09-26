6326d2e43ef91557091cdec5a891d83b3c04a866
thekylemoses <78336025+thekylemoses@users.noreply.github.com>
2022-09-26 05:40:48 -0500

    s

--- static/dice.js
+++ static/dice.js
@@ -1,7 +1,7 @@
 var dice = {
     sides: 6,
     roll: function() {
-      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+      var randomNumber=Math.floor(Math.random()*this.sides)+1;
       return randomNumber;
     }
   }
@@ -10,7 +10,6 @@ var dice = {
     ph.innerHTML = number;
   }
   var button = document.getElementById('button');
-  
   button.onclick = function() {
     var result = dice.roll();
     printNumber(result);
