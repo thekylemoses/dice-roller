aa2af00287dae3ca71e5124bd2304bbb2012ac5f
thekylemoses <78336025+thekylemoses@users.noreply.github.com>
2022-09-26 04:46:58 -0500

    s

--- static/dice.js
+++ static/dice.js
@@ -15,3 +15,20 @@ var dice = {
     printNumber(result);
   };
   
+  var diceTwo = {
+    numSides: 6,
+    roll:function () {
+      var randNum = Math.floor(Math.random()*this.numSides)+1;
+      return randNum;
+    }
+  }
+  function printNumber(number) {
+    var theResult = document.getElementById('theResult').autofocus;
+    theResult.innerHTML = number;
+  }
+  var button = document.getElementById('button');
+  button.onclick = function() {
+    var result = dice.roll();
+    printNumber(result);
+  };
+  
